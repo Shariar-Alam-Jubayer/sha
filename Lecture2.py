2992f3eb3e309127a91335a7fa13769101bd876c
Shariar-Alam-Jubayer <143173801+Shariar-Alam-Jubayer@users.noreply.github.com>
2024-09-22 23:36:29 +0600

    Add files via upload

---
+++ Lecture2.py
@@ -0,0 +1,31 @@
+# str1="sha "
+# str2="riar"
+# print(str2.endswith('ir'))
+# final=str1+str2
+# print(final.count('a'))
+# print(len(final))
+
+# str=input("name :")
+# print(len(str))
+# print(str.find("s"))
+
+# A=int(input("number :"))
+# if(A%2==0):
+#     print("even")
+# else:
+#     print("odd")
+# a=int(input("fFirst number :"))
+# b=int(input("Second number :"))
+# c=int(input("Third number :"))
+# if((a>b) & (a>c)):
+#     print("first nmber is greater",a)
+# elif(b>=c):
+#     print("second number is greater",b)
+# else:
+#     print("third number is greater :",c)
+
+# x=int(input("number :"))
+# if(x%7==0):
+#     print("multiple of 7")
+# else:
+#     print("not multiple of 7")
